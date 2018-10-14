dea87b3edee2fb93007701d0608eb2341b409581
JFTO <fredy.talero@gmail.com>
2018-10-14 15:00:52 -0500

    Ordenar las migraciones

--- api_client.rb
+++ api_client.rb
@@ -1,7 +1,7 @@
 require 'faraday'
 
 
-client = Faraday.new(url: 'http://mysterious-retreat-79142.herokuapp.com/api/v1/stops') do |config|
+client = Faraday.new(url: 'http://info-recolector-rails.herokuapp.com/api/v1/stops') do |config|
     config.adapter Faraday.default_adapter
 end
 
@@ -9,7 +9,7 @@ end
 response = client.post do |req|
     req.url '/api/v1/stops'
     req.headers['Content-Type'] = 'application/json'
-    req.body = '{"stop":{"address": "fedesoft", "latitude": "1245879", "longitude": "12454587"}}'
+    req.body = '{"stop":{"address": "Casa Fredy", "latitude": "987654321", "longitude": "987654321"}}'
 end
 
 #CONSULTA
