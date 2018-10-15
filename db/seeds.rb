ca638e79c45fd67e6ef3038cd51b4004c432293f
JFTO <fredy.talero@gmail.com>
2018-10-14 19:29:46 -0500

    Se crea funcion para un archivo json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
-days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
-for day in days do
-    routes = Week.create({day: day})
-end
+# days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+# for day in days do
+#     routes = Week.create({day: day})
+# end
 
-10.times do
-    type_trucks = TypeTruck.create({name: Faker::Vehicle.make_and_model, description: Faker::Vehicle.standard_specs})
-    type_trucks.trucks.create({name: Faker::Vehicle.make, licensePlate: Faker::Vehicle.style}) 
+# 10.times do
+#     type_trucks = TypeTruck.create({name: Faker::Vehicle.make_and_model, description: Faker::Vehicle.standard_specs})
+#     type_trucks.trucks.create({name: Faker::Vehicle.make, licensePlate: Faker::Vehicle.style}) 
 
-    routes = Route.create({name: Faker::Superhero.name})
-    stops = Stop.create({address: Faker::Address.street_address, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude})
+#     routes = Route.create({name: Faker::Superhero.name})
+#     stops = Stop.create({address: Faker::Address.street_address, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude})
     
-    recycling_points = RecyclingPoint.create({name: Faker::ParksAndRec.character, location: Faker::ParksAndRec.character, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude})  
-    users = User.create({username: Faker::Internet.username, name: Faker::Name.first_name, lastname: Faker::Name.last_name,
-        email: Faker::Internet.email, password: Faker::Internet.password(8)})
+#     recycling_points = RecyclingPoint.create({name: Faker::ParksAndRec.character, location: Faker::ParksAndRec.character, latitude: Faker::Address.latitude, longitude: Faker::Address.longitude})  
+#     users = User.create({username: Faker::Internet.username, name: Faker::Name.first_name, lastname: Faker::Name.last_name,
+#         email: Faker::Internet.email, password: Faker::Internet.password(8)})
+# end
+
+records = JSON.parse(File.read('C:\Users\Talero Caro\Documents\Proyecto Fedesoft\InfoRecolectorRails\users.json'))
+records.each do |record|
+    User.create!(record)
 end
